6742fc745c05368235cfb83c777aa28bc93031f1
Adrian Walker <adrian.walker@bcs.org>
2024-06-09 17:46:54 +0100

    Adding name="mobile-web-app-capable" content="yes"

--- taffnaidphotos.py
+++ taffnaidphotos.py
@@ -16,6 +16,7 @@ LIST_TEMPLATE = """
     <title>taffnaid.photos</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="mobile-web-app-capable" content="yes">
     <link rel="stylesheet" type="text/css" href="/taffnaidphotos.css">
   </head>
   <body>
