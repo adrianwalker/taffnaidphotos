1d9dea9c3501e53859621395cf590204d889e6f7
Adrian Walker <adrian.walker@bcs.org>
2025-01-25 02:00:08 +0000

    Update taffnaidphotos.tf

--- taffnaidphotos.tf
+++ taffnaidphotos.tf
@@ -2,8 +2,8 @@ provider "aws" {
   region = "eu-west-2"
 }
 
-# S3 Bucket
-resource "aws_s3_bucket" "taffnaid_photos" {
+# S3 Bucket for hosting the website
+resource "aws_s3_bucket" "taffnaidphotos" {
   bucket = "taffnaid.photos"
 
   tags = {
@@ -11,9 +11,9 @@ resource "aws_s3_bucket" "taffnaid_photos" {
   }
 }
 
-# Block Public Access
-resource "aws_s3_bucket_public_access_block" "taffnaid_photos" {
-  bucket = aws_s3_bucket.taffnaid_photos.bucket
+# Block public access to the S3 bucket
+resource "aws_s3_bucket_public_access_block" "taffnaidphotos" {
+  bucket = aws_s3_bucket.taffnaidphotos.bucket
 
   block_public_acls       = true
   block_public_policy     = true
@@ -21,9 +21,9 @@ resource "aws_s3_bucket_public_access_block" "taffnaid_photos" {
   restrict_public_buckets = true
 }
 
-# S3 Bucket Website Configuration
-resource "aws_s3_bucket_website_configuration" "taffnaid_photos" {
-  bucket = aws_s3_bucket.taffnaid_photos.bucket
+# Configure the S3 bucket as a static website
+resource "aws_s3_bucket_website_configuration" "taffnaidphotos" {
+  bucket = aws_s3_bucket.taffnaidphotos.bucket
 
   index_document {
     suffix = "index.html"
@@ -34,14 +34,14 @@ resource "aws_s3_bucket_website_configuration" "taffnaid_photos" {
   }
 }
 
-# CloudFront Origin Access Identity (OAI)
-resource "aws_cloudfront_origin_access_identity" "taffnaid_photos" {
+# CloudFront Origin Access Identity (OAI) for secure access to S3
+resource "aws_cloudfront_origin_access_identity" "taffnaidphotos" {
   comment = "OAI for taffnaid.photos"
 }
 
-# S3 Bucket Policy for OAI
-resource "aws_s3_bucket_policy" "taffnaid_photos" {
-  bucket = aws_s3_bucket.taffnaid_photos.bucket
+# S3 Bucket Policy to allow access only via CloudFront
+resource "aws_s3_bucket_policy" "taffnaidphotos" {
+  bucket = aws_s3_bucket.taffnaidphotos.bucket
 
   policy = jsonencode({
     Version = "2012-10-17"
@@ -49,23 +49,23 @@ resource "aws_s3_bucket_policy" "taffnaid_photos" {
       {
         Effect = "Allow"
         Principal = {
-          AWS = aws_cloudfront_origin_access_identity.taffnaid_photos.iam_arn
+          AWS = aws_cloudfront_origin_access_identity.taffnaidphotos.iam_arn
         }
         Action   = "s3:GetObject"
-        Resource = "${aws_s3_bucket.taffnaid_photos.arn}/*"
+        Resource = "${aws_s3_bucket.taffnaidphotos.arn}/*"
       }
     ]
   })
 }
 
 # CloudFront Distribution
-resource "aws_cloudfront_distribution" "taffnaid_photos" {
+resource "aws_cloudfront_distribution" "taffnaidphotos" {
   origin {
-    domain_name = aws_s3_bucket.taffnaid_photos.bucket_regional_domain_name # Use the REST API endpoint
-    origin_id   = aws_s3_bucket.taffnaid_photos.bucket_regional_domain_name
+    domain_name = aws_s3_bucket.taffnaidphotos.bucket_regional_domain_name
+    origin_id   = aws_s3_bucket.taffnaidphotos.bucket_regional_domain_name
 
     s3_origin_config {
-      origin_access_identity = aws_cloudfront_origin_access_identity.taffnaid_photos.cloudfront_access_identity_path
+      origin_access_identity = aws_cloudfront_origin_access_identity.taffnaidphotos.cloudfront_access_identity_path
     }
   }
 
@@ -78,7 +78,7 @@ resource "aws_cloudfront_distribution" "taffnaid_photos" {
   default_cache_behavior {
     allowed_methods  = ["GET", "HEAD"]
     cached_methods   = ["GET", "HEAD"]
-    target_origin_id = aws_s3_bucket.taffnaid_photos.bucket_regional_domain_name
+    target_origin_id = aws_s3_bucket.taffnaidphotos.bucket_regional_domain_name
 
     viewer_protocol_policy = "redirect-to-https"
     compress               = true
@@ -92,8 +92,8 @@ resource "aws_cloudfront_distribution" "taffnaid_photos" {
     }
 
     min_ttl     = 0
-    default_ttl = 3600  # 1 hour
-    max_ttl     = 86400 # 1 day
+    default_ttl = 3600
+    max_ttl     = 86400
   }
 
   restrictions {
@@ -107,10 +107,14 @@ resource "aws_cloudfront_distribution" "taffnaid_photos" {
     ssl_support_method       = "sni-only"
     minimum_protocol_version = "TLSv1.2_2021"
   }
+
+  tags = {
+    Name = "taffnaid.photos"
+  }
 }
 
 # Route 53 Hosted Zone
-resource "aws_route53_zone" "taffnaid_photos" {
+resource "aws_route53_zone" "taffnaidphotos" {
   name = "taffnaid.photos"
 
   tags = {
@@ -118,28 +122,15 @@ resource "aws_route53_zone" "taffnaid_photos" {
   }
 }
 
-# Route 53 A Record (IPv4)
-resource "aws_route53_record" "taffnaid_photos_A" {
-  zone_id = aws_route53_zone.taffnaid_photos.zone_id
+# Route 53 A Record for CloudFront (IPv4)
+resource "aws_route53_record" "taffnaidphotos_A" {
+  zone_id = aws_route53_zone.taffnaidphotos.zone_id
   name    = "taffnaid.photos"
   type    = "A"
 
   alias {
-    name                   = aws_cloudfront_distribution.taffnaid_photos.domain_name
-    zone_id                = aws_cloudfront_distribution.taffnaid_photos.hosted_zone_id
-    evaluate_target_health = false
-  }
-}
-
-# Route 53 AAAA Record (IPv6)
-resource "aws_route53_record" "taffnaid_photos_AAAA" {
-  zone_id = aws_route53_zone.taffnaid_photos.zone_id
-  name    = "taffnaid.photos"
-  type    = "AAAA"
-
-  alias {
-    name                   = aws_cloudfront_distribution.taffnaid_photos.domain_name
-    zone_id                = aws_cloudfront_distribution.taffnaid_photos.hosted_zone_id
+    name                   = aws_cloudfront_distribution.taffnaidphotos.domain_name
+    zone_id                = aws_cloudfront_distribution.taffnaidphotos.hosted_zone_id
     evaluate_target_health = false
   }
 }
